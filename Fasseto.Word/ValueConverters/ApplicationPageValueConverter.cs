8cd924509d5649b29f97f6b6cde200ba63a3e799
Rafael Stunitz <r.stunitz@cinq.com.br>
2019-11-11 19:14:45 -0300

    Add project files.

---
+++ Fasseto.Word/ValueConverters/ApplicationPageValueConverter.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual view/page
+    /// </summary>
+    class ApplicationPageValueConverter : BaseValueConverter<ApplicationPageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Find the appropriate page
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.Login:
+                    return new LoginPage();
+
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
