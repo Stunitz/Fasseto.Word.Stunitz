436ba51e7b12439fd08b7c5a2239852d0f7c2afb
Stunitz <rafaelstunitz@gmail.com>
2019-11-14 20:55:33 -0300

    Added ItemsControl Chat List & Design Time Data and the User Controls Side Menu Content

---
+++ Fasseto.Word/Controls/Chat/ChatListItemControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Interaction logic for ChatListUserControl.xaml
+    /// </summary>
+    public partial class ChatListItemControl : UserControl
+    {
+        public ChatListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
